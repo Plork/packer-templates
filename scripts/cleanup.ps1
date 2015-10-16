85797c7357f97ed91b9936af2ffd46a81f98558b
Plork <marcel@beestig.nl>
2015-10-17 01:02:01 +0200

    silence echoing

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -1,23 +1,23 @@
-# Reduce PageFile size
+Write-Output "Reduce PageFile size"
 $System = GWMI Win32_ComputerSystem -EnableAllPrivileges
 $System.AutomaticManagedPagefile = $False
-$System.Put()
+$result = $System.Put()
 
 $CurrentPageFile = gwmi -query "select * from Win32_PageFileSetting where name='c:\\pagefile.sys'"
 $CurrentPageFile.InitialSize = 512
 $CurrentPageFile.MaximumSize = 512
-$CurrentPageFile.Put()
+$result = $CurrentPageFile.Put()
 
-# Remove unused features
+Write-Output "Removing unused Windows features"
 Remove-WindowsFeature -Name 'Powershell-ISE'
 Get-WindowsFeature |
 ? { $_.InstallState -eq 'Available' } |
 Uninstall-WindowsFeature -Remove
 
-# Cleanup update uninstallers
+Write-Output "Cleanup update uninstallers"
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 
-# Remove logs and temp before Sysprep
+Write-Output "Remove logs and temp before Sysprep"
 @(
     "$env:localappdata\Nuget",
     "$env:localappdata\temp\*",
@@ -33,12 +33,13 @@ Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
         }
     }
 
-# 0ing out empty space
+Write-Output "0ing out empty space"
 wget http://download.sysinternals.com/files/SDelete.zip -OutFile sdelete.zip
 [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
 [System.IO.Compression.ZipFile]::ExtractToDirectory("sdelete.zip", ".")
 
 ./sdelete.exe /accepteula -z c:
 
-mkdir C:\Windows\Panther\Unattend
-copy-item a:\postunattend.xml C:\Windows\Panther\Unattend\unattend.xml
+Write-Output "Copying unattend for sysprep"
+$result = mkdir C:\Windows\Panther\Unattend
+$result = copy-item a:\postunattend.xml C:\Windows\Panther\Unattend\unattend.xml
