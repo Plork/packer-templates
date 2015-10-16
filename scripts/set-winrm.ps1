04255188a19ecadecf37d6079513da82ae5244f8
Plork <marcel@beestig.nl>
2015-10-17 00:03:37 +0200

    changed guest tools to chocolatey and removed unused files

--- scripts/set-winrm.ps1
+++
@@ -1,9 +0,0 @@
-Set-NetFirewallRule -Name WINRM-HTTP-In-TCP-PUBLIC -RemoteAddress Any
-Enable-WSManCredSSP -Force -Role Server
-
-Enable-PSRemoting -Force -SkipNetworkProfileCheck
-winrm set winrm/config '@{MaxTimeoutms="1800000"}'
-winrm set winrm/config/client/auth '@{Basic="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="512"}'
